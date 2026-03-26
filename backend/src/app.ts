6d93f012f5bce68d0141bd13559b92296488569b
Lara Helmli <lara.helmli@gmx.net>
2026-03-26 09:08:37 +0100

    Cors hinzugefügt

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,14 @@
 import express from "express"
+import cors from "cors";
 import { productRouter } from "./route/ProductRoutes";
 
 const app = express();
+
+app.use(
+    cors({
+      origin: "http://localhost:3000",
+    })
+  );
 
 app.use(express.json());
 app.use("/api/products", productRouter);
