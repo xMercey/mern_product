fb8adc0cc842d21c1372be7359786148fe59d6e9
Lara Helmli <lara.helmli@gmx.net>
2026-03-31 16:17:17 +0200

    Dialog DeleteButton

--- frontend/src/components/ProductCard.tsx
+++ frontend/src/components/ProductCard.tsx
@@ -1,4 +1,4 @@
-import { Box, Heading, HStack, IconButton, Image, Text } from "@chakra-ui/react";
+import { Box, Button, CloseButton, Dialog, Heading, HStack, IconButton, Image, Portal, Text } from "@chakra-ui/react";
 import { useColorModeValue } from "./color-mode";
 import { DeleteIcon, EditIcon } from "lucide-react";
 import { deleteProduct } from "./productApi";
@@ -28,25 +28,64 @@ export function ProductCard({product, onDelete}: {product: any; onDelete: (id: s
             <Image src={product.image} alt= {product.name} h={52} w={"full"} objectFit={"cover"}/>
             
             <Box p={4}>
-            <Heading as={"h3"} size={"md"} mb={2}>
-                {product.name}
-            </Heading>
+                <Heading as={"h3"} size={"md"} mb={2}>
+                    {product.name}
+                </Heading>
 
-            <Text fontWeight={"bold"} fontSize={"xl"} color={text} mb={4}>
-                {product.price} Euro
-            </Text>
+                <Text fontWeight={"bold"} fontSize={"xl"} color={text} mb={4}>
+                    {product.price} Euro
+                </Text>
 
-            <HStack>
-                <IconButton aria-label="Produkt bearbeiten">
-                    <EditIcon/>
-                </IconButton>
+                <HStack>
+                    <IconButton aria-label="Produkt bearbeiten">
+                        <EditIcon/>
+                    </IconButton>
 
-                <IconButton aria-label="Produkt löschen" onClick={handleDeleteProduct}>
-                    <DeleteIcon/>
-                </IconButton>             
-            </HStack>
+                <Dialog.Root placement="center" motionPreset="slide-in-bottom">
+                    <Dialog.Trigger asChild>
+                        <IconButton 
+                        aria-label="Produkt löschen" 
+                        bg="red.500"
+                        color="white"
+                        _hover={{ bg: "red.600" }}
+                        >
+                            <DeleteIcon/>
+                        </IconButton>  
+                    </Dialog.Trigger>
 
+                    <Portal>
+                    <Dialog.Backdrop />
+                    <Dialog.Positioner>
+                        <Dialog.Content maxW="xl" mx={4} rounded="2xl">
+                        <Dialog.Header>
+                            <Dialog.Title>Produkt löschen</Dialog.Title>
+                        </Dialog.Header>
+                        <Dialog.Body>
+                            <Text>
+                                Möchtest du <b>{product.name}</b> wirklich löschen?
+                            </Text>
+                        </Dialog.Body>
 
+                        <Dialog.Footer>
+                            <Dialog.ActionTrigger asChild>
+                            <Button variant="outline">Abbrechen</Button>
+                            </Dialog.ActionTrigger>
+
+                            <Button 
+                            colorPalette={"red"}
+                                onClick={handleDeleteProduct}>
+                            Löschen
+                            </Button>
+
+                        </Dialog.Footer>
+                        <Dialog.CloseTrigger asChild>
+                            <CloseButton size="md" />
+                        </Dialog.CloseTrigger>
+                        </Dialog.Content>
+                    </Dialog.Positioner>
+                    </Portal>
+                </Dialog.Root>
+                </HStack>
             </Box>
         </Box>
         </>
