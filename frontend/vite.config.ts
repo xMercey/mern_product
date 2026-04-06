c85341eeb7473f01cb02dcfdaea00f2a0024b75e
Lara Helmli <lara.helmli@gmx.net>
2026-04-06 13:44:29 +0200

    frontend deployment vorbereiten

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,16 +1,10 @@
-import { defineConfig, loadEnv } from 'vite'
-import react from '@vitejs/plugin-react'
-import tsconfigPaths from 'vite-tsconfig-paths'
-import { resolve } from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig(({ mode }) => {
-  // Lädt .env aus dem übergeordneten Ordner
-  const env = loadEnv(mode, resolve(__dirname, '..'), '')
-  
-  return {
-    plugins: [react(), tsconfigPaths()],
-    server: {
-      port: Number(env.VITE_PORT),
-    },
-  }
-})
+export default defineConfig({
+  plugins: [react(), tsconfigPaths()],
+  server: {
+    port: 3000,
+  },
+});
